7845da721c539fbea6773011e34d3e6d66f93d0c
ultraguy007 <ultraguy008@gmail.com>
2015-02-19 21:33:54 +0700

    version 1.001
    add game controller (a little bit) on 19/02/2015

---
+++ Assets/SampleAssets/2D/Scripts/Quest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quest {
+
+    private int score;
+    private int questID;
+    private string questName;
+    private string questDescription;
+    public Quest(int questID, string questName, string questDescription, int score)
+    {
+        this.score = score;
+        this.questID = questID;
+        this.questName = questName;
+        this.questDescription = questDescription;
+    }
+
+    public int Score
+    {
+        set { this.score = Score; }
+        get { return this.score; }
+    }
+
+    public int QuestID
+    {
+        set {  this.questID = QuestID; }
+        get {  return this.questID; }
+    }
+
+    public string QuestName
+    {
+        set {  this.questName = QuestName; }
+        get {  return this.questName; }
+    }
+
+    public string QuestDescription
+    {
+        set {  this.questDescription = QuestDescription; }
+        get { return this.questDescription;  }
+    }
+}
